fa1cbb0b716847bfbeae3ca863f311df5036194e
Steven Belcher <sbelcher@unomaha.edu>
2020-03-30 17:50:24 -0500

    Updates to Profile Page
    Breaking page into components, rearranging and adding dummy data

--- frontend/src/Dashboard/Views/Profile.js
+++ frontend/src/Dashboard/Views/Profile.js
@@ -11,10 +11,10 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import TableFooter from "@material-ui/core/TableFooter";
-import TablePagination from "@material-ui/core/TablePagination";
 
 import { getCurrentUser, updateCurrentUser } from "../../api/users";
+import { UserInfoCard, UserPhysicalCard } from "./ProfileComponents/userinfo";
+import UserItemCard from "./ProfileComponents/useritems";
 
 /**
  * This component contains the UI logic for Profile.
@@ -46,12 +46,29 @@ const dummyUser = {
   state: "NE",
   zip: 68114,
   phone: "(402) 555-5555",
-  gender: null,
+  gender: "Male",
   sports: ["hockey", "baseball"],
   height: 69,
   weight: 160,
   lockerNumber: 115,
   lockerCode: 210315
+};
+
+const dummySizes = {
+  hockey: {
+    head: "M",
+    shirt: "L",
+    pants: 38,
+    socks: "5-12",
+    shoes: 12
+  },
+  baseball: {
+    head: "M",
+    shirt: "L",
+    pants: "36",
+    socks: "5-12",
+    shoes: "11.5"
+  }
 };
 
 const dummyEquipment = [
@@ -91,71 +108,8 @@ const dummyEquipment = [
     value: 14999
   },
   {
-    id: 1,
-    name: "Jersey",
-    size: "L",
-    dueDate: new Date(2020, 11, 17).toDateString(),
-    value: 19995
-  },
-  {
-    id: 2,
-    name: "Padded Pants",
-    size: "38",
-    dueDate: new Date(2020, 11, 17).toDateString(),
-    value: 24998
-  },
-  {
-    id: 3,
-    name: "Socks",
-    size: "5-12",
-    dueDate: new Date(2020, 11, 17).toDateString(),
-    value: 4995
-  },
-  {
-    id: 4,
-    name: "Blades",
-    size: "12",
-    dueDate: new Date(2020, 11, 17).toDateString(),
-    value: 29999
-  },
-  {
-    id: 5,
-    name: "Stick",
-    size: null,
-    dueDate: new Date(2020, 11, 17).toDateString(),
-    value: 14999
-  },
-  {
-    id: 1,
-    name: "Jersey",
-    size: "L",
-    dueDate: new Date(2020, 11, 17).toDateString(),
-    value: 19995
-  },
-  {
-    id: 2,
-    name: "Padded Pants",
-    size: "38",
-    dueDate: new Date(2020, 11, 17).toDateString(),
-    value: 24998
-  },
-  {
-    id: 3,
-    name: "Socks",
-    size: "5-12",
-    dueDate: new Date(2020, 11, 17).toDateString(),
-    value: 4995
-  },
-  {
-    id: 4,
-    name: "Blades",
-    size: "12",
-    dueDate: new Date(2020, 11, 17).toDateString(),
-    value: 29999
-  },
-  {
-    id: 5,
-    name: "Stick",
+    id: 6,
+    name: "Bat",
     size: null,
     dueDate: new Date(2020, 11, 17).toDateString(),
     value: 14999
@@ -163,19 +117,19 @@ const dummyEquipment = [
 ];
 
 export default function Profile(props) {
-  const [firstName, setFirstName] = useState(dummyUser.firstName);
-  const [lastName, setLastName] = useState(dummyUser.lastName);
-  const [email, setEmail] = useState(dummyUser.email);
-  const [username, setUsername] = useState(dummyUser.username);
-  const [address, setAddress] = useState(dummyUser.address);
-  const [city, setCity] = useState(dummyUser.city);
-  const [state, setState] = useState(dummyUser.state);
-  const [zip, setZip] = useState(dummyUser.zip);
-  const [phone, setPhone] = useState(dummyUser.phone);
-  const [gender, setGender] = useState(dummyUser.gender);
-  const [height, setHeight] = useState(dummyUser.height);
-  const [weight, setWeight] = useState(dummyUser.weight);
-  const [name, setName] = useState(firstName + " " + lastName);
+  const firstName = useState(dummyUser.firstName);
+  const lastName = useState(dummyUser.lastName);
+  const email = useState(dummyUser.email);
+  const username = useState(dummyUser.username);
+  const address = useState(dummyUser.address);
+  const city = useState(dummyUser.city);
+  const state = useState(dummyUser.state);
+  const zip = useState(dummyUser.zip);
+  const phone = useState(dummyUser.phone);
+  const gender = dummyUser.gender;
+  const height = dummyUser.height;
+  const weight = dummyUser.weight;
+  const name = useState(firstName[0] + " " + lastName[0]);
   const role = dummyUser.role;
   const lockerNumber = dummyUser.lockerNumber;
   const lockerCode = String(dummyUser.lockerCode);
@@ -185,170 +139,35 @@ export default function Profile(props) {
     return null;
   };
 
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2
-  });
-
-  // TODO: Break this into smaller subcomponents
   return (
     <Grid container spacing={3}>
       <Grid item xs={6}>
-        <Card variant="outlined">
-          <CardContent>
-            <Typography variant="h5" gutterBottom>
-              {role} Information for {name}
-            </Typography>
-            <Grid container spacing={1}>
-              <Grid item xs={6}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  onChange={(e) => setFirstName(e.target.value)}
-                  value={firstName}
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  onChange={(e) => setLastName(e.target.value)}
-                  value={lastName}
-                />
-              </Grid>
-            </Grid>
-            <Grid container spacing={1}>
-              <Grid item xs={6}>
-                <TextField
-                  type="email"
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <TextField
-                  type="tel"
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  id="phone"
-                  label="Phone Number"
-                  value={phone}
-                />
-              </Grid>
-            </Grid>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              id="address"
-              label="Address"
-              onChange={(e) => setAddress(e.target.value)}
-              value={address}
-            />
-            <Grid container spacing={1}>
-              <Grid item xs={6}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  id="city"
-                  label="City"
-                  onChange={(e) => setCity(e.target.value)}
-                  value={city}
-                />
-              </Grid>
-              <Grid item xs={2}>
-                <TextField variant="outlined" margin="normal" fullWidth id="state" label="State" value={state} />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField variant="outlined" margin="normal" fullWidth id="zip" label="Zip" value={zip} />
-              </Grid>
-              <Grid container spacing={1}>
-                <Grid item xs={6}>
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    id="lockerNumber"
-                    label="Locker Number"
-                    value={lockerNumber}
-                    disabled
-                  ></TextField>
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    id="lockerCode"
-                    label="Locker Code"
-                    value={lockerCode.match(/.{2}/g).join("-")}
-                    disabled
-                  ></TextField>
-                </Grid>
-              </Grid>
-              <Grid container spacing={1}></Grid>
-            </Grid>
-          </CardContent>
-        </Card>
+        <UserInfoCard
+          firstName={firstName}
+          lastName={lastName}
+          email={email}
+          username={username}
+          address={address}
+          city={city}
+          state={state}
+          zip={zip}
+          phone={phone}
+          role={role}
+          lockerNumber={lockerNumber}
+          lockerCode={lockerCode}
+        ></UserInfoCard>
       </Grid>
       <Grid item xs={6}>
-        <Card variant="outlined">
-          <CardContent>
-            <Typography variant="h5" gutterBottom>
-              {name}'s Equipment
-            </Typography>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Item</TableCell>
-                  <TableCell>Size</TableCell>
-                  <TableCell>Due Date</TableCell>
-                  <TableCell>Value</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {dummyEquipment.map((item) => (
-                  <TableRow key={item.id}>
-                    <TableCell>{item.name}</TableCell>
-                    <TableCell>{item.size ?? "None"}</TableCell>
-                    <TableCell>{item.dueDate}</TableCell>
-                    <TableCell>{formatter.format(item.value / 100)}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-              <TableFooter>
-                <TablePagination
-                  rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                  colSpan={3}
-                  count={rows.length}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  SelectProps={{
-                    inputProps: { "aria-label": "rows per page" },
-                    native: true
-                  }}
-                  onChangePage={handleChangePage}
-                  onChangeRowsPerPage={handleChangeRowsPerPage}
-                  ActionsComponent={TablePaginationActions}
-                />
-              </TableFooter>
-            </Table>
-          </CardContent>
-        </Card>
+        <UserItemCard name={name} equipment={dummyEquipment}></UserItemCard>
+      </Grid>
+      <Grid item xs={6}>
+        <UserPhysicalCard
+          name={name}
+          height={height}
+          weight={weight}
+          gender={gender}
+          sizes={dummySizes}
+        ></UserPhysicalCard>
       </Grid>
     </Grid>
   );
---
+++ frontend/src/Dashboard/Views/ProfileComponents/userinfo.js
@@ -0,0 +1,248 @@
+import React, { useState } from "react";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import Divider from "@material-ui/core/Divider";
+import FormControl from "@material-ui/core/FormControl";
+import Button from "@material-ui/core/Button";
+
+function UserInfoCard(props) {
+  const {
+    firstName,
+    lastName,
+    email,
+    username,
+    address,
+    city,
+    state,
+    zip,
+    phone,
+    role,
+    lockerNumber,
+    lockerCode
+  } = props;
+  const name = useState(firstName[0] + " " + lastName[0]);
+
+  return (
+    <Card variant="outlined">
+      <CardContent>
+        <Typography variant="h5" gutterBottom>
+          {role} Information for {name}
+        </Typography>
+        <Grid container spacing={1}>
+          <Grid item xs={6}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="firstName"
+              label="First Name"
+              onChange={(e) => firstName[1](e.target.value)}
+              value={firstName[0]}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="lastName"
+              label="Last Name"
+              onChange={(e) => lastName[1](e.target.value)}
+              value={lastName[0]}
+            />
+          </Grid>
+        </Grid>
+        <Grid container spacing={1}>
+          <Grid item xs={6}>
+            <TextField
+              type="email"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="email"
+              label="Email Address"
+              onChange={(e) => email[1](e.target.value)}
+              value={email[0]}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              type="tel"
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="phone"
+              label="Phone Number"
+              onChange={(e) => phone[1](e.target.value)}
+              value={phone[0]}
+            />
+          </Grid>
+        </Grid>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          id="address"
+          label="Address"
+          onChange={(e) => address[1](e.target.value)}
+          value={address[0]}
+        />
+        <Grid container spacing={1}>
+          <Grid item xs={6}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="city"
+              label="City"
+              onChange={(e) => city[1](e.target.value)}
+              value={city[0]}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="state"
+              label="State"
+              onChange={(e) => state[1](e.target.value)}
+              value={state[0]}
+            />
+          </Grid>
+          <Grid item xs={4}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="zip"
+              label="Zip"
+              onChange={(e) => zip[1](e.target.value)}
+              value={zip[0]}
+            />
+          </Grid>
+          <Grid container spacing={1}>
+            <Grid item xs={6}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="lockerNumber"
+                label="Locker Number"
+                value={lockerNumber}
+                disabled
+              ></TextField>
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="lockerCode"
+                label="Locker Code"
+                value={lockerCode.match(/.{2}/g).join("-")}
+                disabled
+              ></TextField>
+            </Grid>
+          </Grid>
+          <Grid container spacing={1}></Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+}
+
+function UserPhysicalCard(props) {
+  const { name, height, weight, gender, sizes } = props;
+  const hockeySizes = sizes.hockey; // Loading dummy data for now
+  const [head, setHead] = useState(hockeySizes.head);
+  const [shirt, setShirt] = useState(hockeySizes.shirt);
+  const [pants, setPants] = useState(hockeySizes.pants);
+  const [socks, setSocks] = useState(hockeySizes.socks);
+  const [shoes, setShoes] = useState(hockeySizes.shoes);
+
+  return (
+    <FormControl style={{ minWidth: 120 }}>
+      <Card variant="outlined" my={5}>
+        <CardContent>
+          <Typography variant="h5" gutterBottom>
+            Size Information for {name}
+          </Typography>
+          <Grid container spacing={1}>
+            <Grid item xs={3}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="height"
+                label="Height (inches)"
+                value={height}
+                disabled
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="weight"
+                label="Weight (lbs)"
+                value={weight}
+                disabled
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                id="gender"
+                label="Gender"
+                value={gender}
+                disabled
+              />
+            </Grid>
+          </Grid>
+          <br />
+          <Divider />
+          <br />
+          <Select id="head" value={head} onChange={setHead}>
+            <MenuItem value={"S"}>Small</MenuItem>
+            <MenuItem value={"M"}>Medium</MenuItem>
+            <MenuItem value={"L"}>Large</MenuItem>
+            <MenuItem value={"XL"}>Extra Large</MenuItem>
+          </Select>
+          <FormHelperText>Head Size</FormHelperText>
+          <Select id="shirt" value={shirt} onChange={setHead}>
+            <MenuItem value={"S"}>Small</MenuItem>
+            <MenuItem value={"M"}>Medium</MenuItem>
+            <MenuItem value={"L"}>Large</MenuItem>
+            <MenuItem value={"XL"}>Extra Large</MenuItem>
+          </Select>
+          <FormHelperText>Shirt Size</FormHelperText>
+          <TextField id="pants" value={pants} onChange={setPants}></TextField>
+          <FormHelperText>Pant Size</FormHelperText>
+          <Select id="socks" value={socks} onChange={setSocks}>
+            <MenuItem value={"5-12"}>5-12</MenuItem>
+            <MenuItem value={"13-20"}>13-20</MenuItem>
+          </Select>
+          <FormHelperText>Sock Size</FormHelperText>
+          <Select id="shoes" value={shoes} onChange={setShoes}>
+            {[...Array(21).keys()].map((size) => (
+              <MenuItem value={size}>{size}</MenuItem>
+            ))}
+          </Select>
+          <FormHelperText>Shoe Size</FormHelperText>
+        </CardContent>
+      </Card>
+    </FormControl>
+  );
+}
+
+export { UserInfoCard, UserPhysicalCard };
