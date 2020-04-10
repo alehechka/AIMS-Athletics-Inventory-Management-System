e509d4bf851778ebbdf7084e9a0d7577e3642e94
donosmith <43357723+donosmith@users.noreply.github.com>
2020-04-10 16:58:36 -0500

    First two pages of stepper logic on transaction page

--- frontend/src/Dashboard/Views/Transaction.js
+++ frontend/src/Dashboard/Views/Transaction.js
@@ -14,246 +14,168 @@ import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
+import Stepper from "@material-ui/core/Stepper";
+import Step from "@material-ui/core/Step";
+import StepLabel from "@material-ui/core/StepLabel";
+import MaterialTable from "material-table";
+import Chip from "@material-ui/core/Chip";
+import Icon from "@material-ui/core/Icon";
 
 import { SportsAPI, InventoryAPI, UsersAPI } from "../../api";
+import { StepContent } from "@material-ui/core";
 
 export default function Transaction(props) {
-  const [users, setUsers] = React.useState([]);
-  const [usersMaster, setUsersMaster] = React.useState([]);
-  const [usersCheckbox, setUsersCheckbox] = React.useState([]);
 
-  const [sports, setSports] = React.useState([]);
-  const [sportsFilter, setSportsFilter] = React.useState([]);
+    const [usersSelected, setUsersSelected] = React.useState([]);
+    const [userColumns, updateUserColumns] = React.useState([]);
+    const [userData, updateUserData] = React.useState([]);
+    const [isUserLoading, updateUserLoading] = React.useState(true);
 
-  const [teams, setTeams] = React.useState([]);
+    const [inventorySelected, setInventorySelected] = React.useState([]);
+    const [inventoryColumns, updateInventoryColumns] = React.useState([]);
+    const [inventoryData, updateInventoryData] = React.useState([]);
+    const [isInventoryLoading, updateInventoryLoading] = React.useState(true);
 
-  const [inventory, setInventory] = React.useState([]);
-  const [inventoryCheckbox, setInventoryCheckbox] = React.useState([]);
-  const [checkedInventory, setCheckedInventory] = React.useState([]);
+    React.useEffect(() => {
 
-  const [transactions, setTransactions] = React.useState([]);
+        UsersAPI.getUsers(null, null, { isAdmin: true, isEmployee: true, isCoach: true, isAthlete: true }).then((users) => {
+          updateUserColumns([
+            {title: 'ID', field: 'id', hidden: true},
+            {title: 'Sport', field: 'sportsJson', hidden: true},
+            {title: 'First Name', field: 'firstName'},
+            {title: 'Last Name', field: 'lastName'},
+            {title: 'Sport(s)', field: 'sports',
+                render: rowData => rowData.sports.map((val, index) =>
+                    <Chip key={index} 
+                        label={val.displayName} style ={{margin: 2}}
+                        icon={<Icon>{val.icon}</Icon>}>
+                    </Chip>),
+                customFilterAndSearch: (term, rowData) => 
+                    rowData.sports
+                    .map(val => val.displayName)
+                    .some(val => val.toLowerCase().includes(term.toLowerCase())),
+                cellStyle: {width:"100%"}
+            }
+            ]);
+            const customData = users.map(user =>({
+                id: user.id,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                sportsJson: JSON.stringify(user.sports),
+                sports: user.sports
+            }));
+            updateUserData(customData);
+            updateUserLoading(false);
+        });
+    
+        //setTeams([{id: 1, name: "The Ball Boys"}, {id:2, name: "Sportsmen"}]);
+    
+        InventoryAPI.getInventory(null, null, {}).then((inventory) => {
+          updateInventoryColumns([
+            {title: 'ID', field: 'id', hidden: true},
+            {title: 'Name', field: 'name'},
+            {title: 'Description', field: 'description'},
+            {title: 'Quantity', field: 'quantity'}
+            ]);
+            const customData = inventory.map(inventory =>({
+                id: inventory.id,
+                name: inventory.name,
+                description: inventory.description,
+                quantity: inventory.totalQuantity
+            }));
+            updateInventoryData(customData);
+            updateInventoryLoading(false);
+        });
+      }, []);
 
-  const handleSportFilter = (event) => {
-    var tempUsers = JSON.parse(JSON.stringify(usersMaster));
-    setSportsFilter(event.target.value);
-    setUsers(tempUsers.filter((user) => user.sports.find((sports) => sports.name.includes(event.target.value))));
-    console.log(sportsFilter, setTeams([]));
-  };
+      function getSteps() {
+        return ['Select Users', 'Select Inventory', 'Edit Transactions'];
+      }
 
-  const handleAthleteToggle = (value) => () => {
-    const currentIndex = usersCheckbox.indexOf(value);
-    const newUsersCheckbox = [...usersCheckbox];
-    const newCheckedUser = usersMaster.find((user) => user.id === value);
+      function getStepContent(stepIndex) {
+            switch (stepIndex) {
+            case 0:
+                return(       
+                    <div style={{ maxWidth: '100%' }}>
+                        <MaterialTable
+                        title="Users"
+                        isLoading = {isUserLoading}
+                        columns={userColumns}
+                        data={userData}
+                        options={{selection : true}}
+                        onSelectionChange= {(rows) => setUsersSelected(rows)}
+                        />
+                </div>)
+            case 1:
+                return(       
+                    <div style={{ maxWidth: '100%' }}>
+                        <MaterialTable
+                        title="Inventory"
+                        isLoading = {isInventoryLoading}
+                        columns={inventoryColumns}
+                        data={inventoryData}
+                        options={{selection : true}}
+                        onSelectionChange= {(rows) => setInventorySelected(rows)}
+                        />
+                </div>)
+            case 2:
+                return <Typography>Test2</Typography>;
+            default:
+                return <Typography>Test</Typography>;
+            }
+        }
 
-    const newTransactions = [...transactions];
-    let newTransaction = { id: newCheckedUser.id, fullName: newCheckedUser.fullName, inventory: [] };
+      //STEPPER CONFIG
+      const [activeStep, setActiveStep] = React.useState(0);
+      const steps = getSteps();
+    
+      const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      };
+    
+      const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+      };
+    
+      const handleReset = () => {
+        setActiveStep(0);
+      };
+      //
 
-    if (currentIndex === -1) {
-      newUsersCheckbox.push(value);
-      newTransactions.push(newTransaction);
-    } else {
-      newUsersCheckbox.splice(currentIndex, 1);
-      newTransactions.splice(currentIndex, 1);
-    }
-
-    setUsersCheckbox(newUsersCheckbox);
-    setTransactions(newTransactions);
-
-    //console.log(newTransactions);
-  };
-
-  const handleInventoryToggle = (value) => () => {
-    //console.log(transactions);
-    const currentIndex = inventoryCheckbox.indexOf(value);
-    const newInventoryCheckbox = [...inventoryCheckbox];
-    const newCheckedInventory = [...checkedInventory];
-    const newCheckedInventoryItem = inventory.find((inventory) => inventory.id === value);
-
-    if (currentIndex === -1) {
-      newInventoryCheckbox.push(value);
-      newCheckedInventory.push(newCheckedInventoryItem);
-    } else {
-      newInventoryCheckbox.splice(currentIndex, 1);
-      newCheckedInventory.splice(currentIndex, 1);
-    }
-
-    //console.log(newCheckedInventory);
-
-    let temp = [];
-
-    newCheckedInventory.forEach((inventory) => temp.push({ id: inventory.id, name: inventory.name }));
-    transactions.forEach((transaction) => (transaction.inventory = temp));
-
-    //console.log(temp);
-
-    setInventoryCheckbox(newInventoryCheckbox);
-    setCheckedInventory(newCheckedInventory);
-    setTransactions(transactions);
-
-    //console.log(transactions);
-  };
-
-  const handleTransactionToggle = (transactionId, inventoryId) => () => {
-    var currentTransaction = transactions.find((transaction) => transaction.id === transactionId);
-    var indexOfCurrentTransaction = transactions.indexOf(currentTransaction);
-    var itemToFind = currentTransaction.inventory.find((inventory) => inventory.id === inventoryId);
-    var itemExistsInTransaction = currentTransaction.inventory.includes(itemToFind);
-
-    let newTransactions = transactions;
-
-    var inventoryDeepCopy = JSON.parse(JSON.stringify(newTransactions[indexOfCurrentTransaction].inventory));
-
-    if (itemExistsInTransaction) {
-      var indexToRemove = currentTransaction.inventory.indexOf(itemToFind);
-
-      inventoryDeepCopy.splice(indexToRemove, 1);
-
-      newTransactions[indexOfCurrentTransaction].inventory = inventoryDeepCopy;
-    } else {
-      var itemToAdd = inventory.find((inventory) => inventory.id === inventoryId);
-
-      inventoryDeepCopy.push(itemToAdd);
-
-      newTransactions[indexOfCurrentTransaction].inventory = inventoryDeepCopy;
-    }
-
-    setTransactions(newTransactions);
-  };
-
-  const submit = () => {
-    console.log("submit");
-    window.location.reload(true);
-  };
-
-  React.useEffect(() => {
-    SportsAPI.getSports().then((sports) => {
-      setSports(sports);
-    });
-
-    UsersAPI.getUsers(null, null, { isAdmin: true, isEmployee: true, isCoach: true, isAthlete: true }).then((users) => {
-      setUsers(users);
-      setUsersMaster(users);
-    });
-
-    //setTeams([{id: 1, name: "The Ball Boys"}, {id:2, name: "Sportsmen"}]);
-
-    InventoryAPI.getInventory(null, null, {}).then((inventory) => {
-      setInventory(inventory);
-    });
-  }, []);
-
-  return (
-    <div style={{ maxWidth: "100%", marginLeft: "10px", marginRight: "10px", marginBottom: "10px" }}>
-      <Grid item xs={12}>
-        <Card variant="outlined">
-          <CardContent>
-            <Grid container spacing={4}>
-              <Grid item xs={1}>
-                <Typography>Sports</Typography>
-                <Select style={{ minWidth: 120 }} defaultValue={""} onChange={handleSportFilter}>
-                  {sports.map((sport) => (
-                    <MenuItem key={sport.id} value={sport.name}>
-                      {sport.displayName}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </Grid>
-              <Grid item xs={2}>
-                <Typography>Teams</Typography>
-                <List>
-                  {teams.map((team) => {
-                    return (
-                      <ListItem key={team.id} dense>
-                        <ListItemText primary={team.name}></ListItemText>
-                        <ListItemIcon>
-                          <Checkbox edge="start" />
-                        </ListItemIcon>
-                      </ListItem>
-                    );
-                  })}
-                </List>
-              </Grid>
-              <Grid item xs={3}>
-                <Typography>Athletes</Typography>
-                <List style={{ overflow: "auto", maxHeight: 250 }}>
-                  {users.map((user) => {
-                    return (
-                      <ListItem key={user.id} dense button onClick={handleAthleteToggle(user.id)}>
-                        <ListItemText primary={user.fullName} secondary={user.isActive} />
-                        <ListItemIcon>
-                          <Checkbox edge="start" checked={usersCheckbox.indexOf(user.id) !== -1} />
-                        </ListItemIcon>
-                      </ListItem>
-                    );
-                  })}
-                </List>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography>Inventory</Typography>
-                <List style={{ overflow: "auto", maxHeight: 250 }}>
-                  {inventory.map((inventory) => {
-                    return (
-                      <ListItem key={inventory.id} dense button onClick={handleInventoryToggle(inventory.id)}>
-                        <ListItemText primary={inventory.name} secondary={inventory.description} />
-                        <ListItemIcon>
-                          {/*<Checkbox
-                                            edge="start"
-                                            checked={inventoryCheckbox.indexOf(inventory.id) !== -1}
-                                        />*/}
-                        </ListItemIcon>
-                      </ListItem>
-                    );
-                  })}
-                </List>
-              </Grid>
-            </Grid>
-          </CardContent>
-        </Card>
-        <Grid item xs={12}>
-          {transactions.map((transaction) => (
-            <Card variant="outlined" key={transaction.id}>
-              <CardContent>
-                <Grid container spacing={2}>
-                  <Grid item xs={2}>
-                    <Typography>Athlete</Typography>
-                    <Typography>{transaction.fullName}</Typography>
-                  </Grid>
-                  <Grid item xs={3}>
-                    <Typography>Inventory</Typography>
-                    <List style={{ overflow: "auto", maxHeight: 250 }}>
-                      {transaction.inventory.map((inventory) => {
-                        return (
-                          <ListItem key={inventory.id} dense>
-                            <ListItemText primary={inventory.name}></ListItemText>
-                            <ListItemIcon>
-                              <Checkbox
-                                edge="start"
-                                onChange={handleTransactionToggle(transaction.id, inventory.id)}
-                                defaultChecked
-                              />
-                            </ListItemIcon>
-                          </ListItem>
-                        );
-                      })}
-                    </List>
-                  </Grid>
-                  <Grid item xs={2}>
-                    <IconButton aria-label="delete" onClick={handleAthleteToggle(transaction.id)}>
-                      <CloseIcon />
-                    </IconButton>
-                  </Grid>
-                </Grid>
-              </CardContent>
-            </Card>
+    return(
+        <div>
+        <Typography>Transactions</Typography>
+        <Stepper activeStep={activeStep} alternativeLabel>
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
           ))}
-          {/*<Button onClick = {() => console.log(transactions)}>Print Transactions</Button>*/}
-          <Box display="flex" marginTop={5} justifyContent="center">
-            <Button variant="contained" disableElevation onClick={() => submit()}>
-              Submit
-            </Button>
-          </Box>
-        </Grid>
-      </Grid>
-    </div>
-  );
+        </Stepper>
+        <div>
+          {activeStep === steps.length ? (
+            <div>
+              <Typography>All steps completed</Typography>
+              <Button onClick={handleReset}>Reset</Button>
+            </div>
+          ) : (
+            <div>
+              {getStepContent(activeStep)}
+              <div>
+                <Button
+                  disabled={activeStep === 0}
+                  onClick={handleBack}
+                >
+                  Back
+                </Button>
+                <Button variant="contained" color="primary" onClick={handleNext}>
+                  {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                </Button>
+
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    )
 }
